e283f8485997fd9ee02e4c9b0fb6d0f657e97b80
Riham Bassim <riham@Rihams-Laptop.local>
2024-05-28 19:08:07 +0200

    REACT-03_header

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -1,17 +1,27 @@
-import Card from "react-bootstrap/Card";
+import "./header.scss";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function HeaderComponent() {
   return (
-    <Card className="bg-dark text-white">
-      <Card.Img src="" alt="Card image" />
-      <Card.ImgOverlay>
-        <Card.Title>Card title</Card.Title>
-        <Card.Text>
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </Card.Text>
-        <Card.Text>Last updated 3 mins ago</Card.Text>
-      </Card.ImgOverlay>
-    </Card>
+    <div className="page-container">
+      <Container className="h-100 ">
+        <Row className="d-flex justify-content-center align-items-center ">
+          <div className="header-description text-center text-secondary">
+            RiWatch : Votre portail vers des millions de films, séries et
+            célébrités. Plongez dans l'univers du cinéma !
+          </div>
+        </Row>
+
+        <Row className="header-button d-flex justify-content-center align-items-center ">
+          <Button>Découvrir</Button>
+        </Row>
+      </Container>
+    </div>
   );
 }
