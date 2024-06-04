4e732c8b73fc458071408daf15ee2dc4fde83b47
Riham Bassim <riham@Rihams-Laptop.local>
2024-06-04 20:38:54 +0200

    REACT-12_composants-dynamiques-propsREACT-11_barre-recherche-pageNotfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,13 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import FooterComponent from "../components/common/footer/FooterComponent";
+import { useEffect } from "react";
 export default function NotFound() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  }, []);
   return (
     <>
       <h1>NotFound</h1>
